640767195e9936499be81089762ce7eeafe877a1
Arnav Singh <165240307+arnavsingh0@users.noreply.github.com>
2026-03-06 10:15:18 -0500

    Add today.py

---
+++ today.py
@@ -0,0 +1,269 @@
+import datetime
+from dateutil import relativedelta
+import requests
+import os
+from lxml import etree
+import time
+import hashlib
+
+HEADERS = {'authorization': 'token '+ os.environ['ACCESS_TOKEN']}
+USER_NAME = os.environ['USER_NAME']
+QUERY_COUNT = {'user_getter': 0, 'follower_getter': 0, 'graph_repos_stars': 0, 'recursive_loc': 0, 'graph_commits': 0, 'loc_query': 0}
+
+def simple_request(func_name, query, variables):
+    request = requests.post('https://api.github.com/graphql', json={'query': query, 'variables':variables}, headers=HEADERS)
+    if request.status_code == 200:
+        return request
+    raise Exception(func_name, ' has failed with a', request.status_code, request.text, QUERY_COUNT)
+
+def graph_repos_stars(count_type, owner_affiliation, cursor=None, add_loc=0, del_loc=0):
+    query_count('graph_repos_stars')
+    query = '''
+    query ($owner_affiliation: [RepositoryAffiliation], $login: String!, $cursor: String) {
+        user(login: $login) {
+            repositories(first: 100, after: $cursor, ownerAffiliations: $owner_affiliation) {
+                totalCount
+                edges {
+                    node {
+                        ... on Repository {
+                            nameWithOwner
+                            stargazers {
+                                totalCount
+                            }
+                        }
+                    }
+                }
+                pageInfo {
+                    endCursor
+                    hasNextPage
+                }
+            }
+        }
+    }'''
+    variables = {'owner_affiliation': owner_affiliation, 'login': USER_NAME, 'cursor': cursor}
+    request = simple_request(graph_repos_stars.__name__, query, variables)
+    if request.status_code == 200:
+        if count_type == 'repos':
+            return request.json()['data']['user']['repositories']['totalCount']
+        elif count_type == 'stars':
+            return sum(node['node']['stargazers']['totalCount'] for node in request.json()['data']['user']['repositories']['edges'])
+
+def recursive_loc(owner, repo_name, data, cache_comment, addition_total=0, deletion_total=0, my_commits=0, cursor=None):
+    query_count('recursive_loc')
+    query = '''
+    query ($repo_name: String!, $owner: String!, $cursor: String) {
+        repository(name: $repo_name, owner: $owner) {
+            defaultBranchRef {
+                target {
+                    ... on Commit {
+                        history(first: 100, after: $cursor) {
+                            totalCount
+                            edges {
+                                node {
+                                    ... on Commit {
+                                        committedDate
+                                    }
+                                    author {
+                                        user {
+                                            id
+                                        }
+                                    }
+                                    deletions
+                                    additions
+                                }
+                            }
+                            pageInfo {
+                                endCursor
+                                hasNextPage
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }'''
+    variables = {'repo_name': repo_name, 'owner': owner, 'cursor': cursor}
+    request = requests.post('https://api.github.com/graphql', json={'query': query, 'variables':variables}, headers=HEADERS)
+    if request.status_code == 200:
+        if request.json()['data']['repository']['defaultBranchRef'] != None:
+            history = request.json()['data']['repository']['defaultBranchRef']['target']['history']
+            for node in history['edges']:
+                if node['node']['author']['user'] is not None and node['node']['author']['user']['id'] == OWNER_ID['id']:
+                    my_commits += 1
+                    addition_total += node['node']['additions']
+                    deletion_total += node['node']['deletions']
+
+            if history['edges'] == [] or not history['pageInfo']['hasNextPage']:
+                return addition_total, deletion_total, my_commits
+            else: return recursive_loc(owner, repo_name, data, cache_comment, addition_total, deletion_total, my_commits, history['pageInfo']['endCursor'])
+        else: return 0, 0, 0
+    force_close_file(data, cache_comment)
+    if request.status_code == 403:
+        raise Exception('Too many requests. Hit anti-abuse limit.')
+    raise Exception('recursive_loc() failed', request.status_code, request.text, QUERY_COUNT)
+
+def loc_query(owner_affiliation, comment_size=0, force_cache=False, cursor=None, edges=[]):
+    query_count('loc_query')
+    query = '''
+    query ($owner_affiliation: [RepositoryAffiliation], $login: String!, $cursor: String) {
+        user(login: $login) {
+            repositories(first: 60, after: $cursor, ownerAffiliations: $owner_affiliation) {
+            edges {
+                node {
+                    ... on Repository {
+                        nameWithOwner
+                        defaultBranchRef {
+                            target {
+                                ... on Commit {
+                                    history { totalCount }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            pageInfo { endCursor hasNextPage }
+            }
+        }
+    }'''
+    variables = {'owner_affiliation': owner_affiliation, 'login': USER_NAME, 'cursor': cursor}
+    request = simple_request(loc_query.__name__, query, variables)
+    edges_data = request.json()['data']['user']['repositories']['edges']
+    edges.extend(edges_data)
+    
+    if request.json()['data']['user']['repositories']['pageInfo']['hasNextPage']:
+        return loc_query(owner_affiliation, comment_size, force_cache, request.json()['data']['user']['repositories']['pageInfo']['endCursor'], edges)
+    else:
+        return cache_builder(edges, comment_size, force_cache)
+
+def cache_builder(edges, comment_size, force_cache, loc_add=0, loc_del=0):
+    cached = True
+    os.makedirs('cache', exist_ok=True)
+    filename = 'cache/'+hashlib.sha256(USER_NAME.encode('utf-8')).hexdigest()+'.txt'
+    try:
+        with open(filename, 'r') as f:
+            data = f.readlines()
+    except FileNotFoundError:
+        data = []
+        for _ in range(comment_size): data.append('Comment Block\n')
+        with open(filename, 'w') as f: f.writelines(data)
+
+    if len(data)-comment_size != len(edges) or force_cache:
+        cached = False
+        flush_cache(edges, filename, comment_size)
+        with open(filename, 'r') as f: data = f.readlines()
+
+    cache_comment = data[:comment_size]
+    data = data[comment_size:]
+    for index in range(len(edges)):
+        parts = data[index].split()
+        if len(parts) >= 5:
+            repo_hash, commit_count = parts[0], parts[1]
+            if repo_hash == hashlib.sha256(edges[index]['node']['nameWithOwner'].encode('utf-8')).hexdigest():
+                try:
+                    if int(commit_count) != edges[index]['node']['defaultBranchRef']['target']['history']['totalCount']:
+                        owner, repo_name = edges[index]['node']['nameWithOwner'].split('/')
+                        loc = recursive_loc(owner, repo_name, data, cache_comment)
+                        if isinstance(loc, tuple):
+                            data[index] = f"{repo_hash} {edges[index]['node']['defaultBranchRef']['target']['history']['totalCount']} {loc[2]} {loc[0]} {loc[1]}\n"
+                except (TypeError, KeyError):
+                    data[index] = f"{repo_hash} 0 0 0 0\n"
+    with open(filename, 'w') as f:
+        f.writelines(cache_comment)
+        f.writelines(data)
+    for line in data:
+        loc = line.split()
+        if len(loc) >= 5:
+            loc_add += int(loc[3])
+            loc_del += int(loc[4])
+    return [loc_add, loc_del, loc_add - loc_del, cached]
+
+def flush_cache(edges, filename, comment_size):
+    with open(filename, 'r') as f:
+        data = f.readlines()[:comment_size] if comment_size > 0 else []
+    with open(filename, 'w') as f:
+        f.writelines(data)
+        for node in edges:
+            f.write(hashlib.sha256(node['node']['nameWithOwner'].encode('utf-8')).hexdigest() + ' 0 0 0 0\n')
+
+def force_close_file(data, cache_comment):
+    filename = 'cache/'+hashlib.sha256(USER_NAME.encode('utf-8')).hexdigest()+'.txt'
+    with open(filename, 'w') as f:
+        f.writelines(cache_comment)
+        f.writelines(data)
+
+def svg_overwrite(filename, commit_data, star_data, repo_data, follower_data, loc_data):
+    tree = etree.parse(filename)
+    root = tree.getroot()
+    justify_format(root, 'commit_data', commit_data)
+    justify_format(root, 'star_data', star_data)
+    justify_format(root, 'repo_data', repo_data)
+    justify_format(root, 'follower_data', follower_data)
+    justify_format(root, 'loc_data', loc_data[2])
+    justify_format(root, 'loc_add', f"{loc_data[0]}++")
+    justify_format(root, 'loc_del', f"{loc_data[1]}--")
+    tree.write(filename, encoding='utf-8', xml_declaration=True)
+
+def justify_format(root, element_id, new_text):
+    if isinstance(new_text, int):
+        new_text = f"{'{:,}'.format(new_text)}"
+    new_text = str(new_text)
+    find_and_replace(root, element_id, new_text)
+
+def find_and_replace(root, element_id, new_text):
+    element = root.find(f".//*[@id='{element_id}']")
+    if element is not None:
+        element.text = new_text
+
+def commit_counter(comment_size):
+    total_commits = 0
+    filename = 'cache/'+hashlib.sha256(USER_NAME.encode('utf-8')).hexdigest()+'.txt'
+    with open(filename, 'r') as f: data = f.readlines()[comment_size:]
+    for line in data:
+        parts = line.split()
+        if len(parts) >= 3:
+            total_commits += int(parts[2])
+    return total_commits
+
+def user_getter(username):
+    query_count('user_getter')
+    query = '''
+    query($login: String!){
+        user(login: $login) {
+            id
+            createdAt
+        }
+    }'''
+    request = simple_request(user_getter.__name__, query, {'login': username})
+    return {'id': request.json()['data']['user']['id']}, request.json()['data']['user']['createdAt']
+
+def follower_getter(username):
+    query_count('follower_getter')
+    query = '''
+    query($login: String!){
+        user(login: $login) {
+            followers { totalCount }
+        }
+    }'''
+    request = simple_request(follower_getter.__name__, query, {'login': username})
+    return int(request.json()['data']['user']['followers']['totalCount'])
+
+def query_count(funct_id):
+    global QUERY_COUNT
+    QUERY_COUNT[funct_id] += 1
+
+if __name__ == '__main__':
+    user_data, user_time = user_getter(USER_NAME), 0
+    OWNER_ID, acc_date = user_data[0], user_data[1]
+    
+    total_loc = loc_query(['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'], 7)
+    commit_data = commit_counter(7)
+    star_data = graph_repos_stars('stars', ['OWNER'])
+    repo_data = graph_repos_stars('repos', ['OWNER'])
+    follower_data = follower_getter(USER_NAME)
+
+    for index in range(len(total_loc)-1): total_loc[index] = '{:,}'.format(total_loc[index])
+    
+    # Overwrite the SVG
+    svg_overwrite('dark_mode.svg', commit_data, star_data, repo_data, follower_data, total_loc[:-1])
+    print("SVG Successfully Updated.")
